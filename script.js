95d0fb8b295cbb1a3f32d86b37542eb59695b1f8
etim-ipee <imaikopelijah011@gmail.com>
2026-06-23 09:04:18 -0700

    Add array practice task

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+
 let myNmae = "John"; //Wrog variable name
 let myName = "Johnn"; //corrected
 
-//Variables and Personal Information
+//Task 7: Variables and Personal Information
 
 let firstName = "Elijah";
 let lastName = "Imaikop";
@@ -10,7 +11,7 @@ let favoriteLanguage = "JavaScript";
 
 console.log(`Hi, my name is ${firstName} ${lastName}. I am ${age} years old and my favorite programming language is ${favoriteLanguage}.`);
 
-//Simple calculator
+//Task 8: Simple calculator
 let num1 = 10;
 let num2 = 3;
 
@@ -20,7 +21,7 @@ console.log("Multiplication:", num1 * num2);
 console.log("Division:", num1 / num2);
 console.log("Remainder:", num1 % num2);
 
-//Age Checker
+//Task 9:Age Checker
 let ageCheck = 17;
 
 if (ageCheck >= 18) {
@@ -28,4 +29,25 @@ if (ageCheck >= 18) {
 } else {
   console.log("You are not eligible yet");
 }
+
+//Task 10:Simple Function 
+function greetUser(name) {
+  console.log(`Hello, ${name}. Welcome to JavaScript.`);
+}
+
+greetUser("Elijah");
+greetUser("Sarah");
+greetUser("Michael");
+
+//Task 11:Array Practice
+
+let languages = ["JavaScript", "Python", "C++", "Java", "Ruby"];
+
+console.log("First language:", languages[0]);
+console.log("Last language:", languages[languages.length - 1]);
+
+languages.push("Go"); // Add new language
+languages.pop(); // Remove last language
+
+console.log("Full array:", languages);
 
