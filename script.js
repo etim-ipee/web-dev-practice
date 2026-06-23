d4cd7cbbc3bdd2de375ae5307ff90c6a0acd61fb
etim-ipee <imaikopelijah011@gmail.com>
2026-06-23 08:30:40 -0700

    Add age checker condition

--- script.js
+++ script.js
@@ -20,4 +20,12 @@ console.log("Multiplication:", num1 * num2);
 console.log("Division:", num1 / num2);
 console.log("Remainder:", num1 % num2);
 
+//Age Checker
+let ageCheck = 17;
+
+if (ageCheck >= 18) {
+  console.log("You are eligible");
+} else {
+  console.log("You are not eligible yet");
+}
 
