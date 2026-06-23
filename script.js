729bb17d7172a91ffa17196940ac59b693abee48
etim-ipee <imaikopelijah011@gmail.com>
2026-06-23 08:13:07 -0700

    Add personal information variables

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 let myNmae = "John"; //Wrog variable name
 let myName = "Johnn"; //corrected
+
+//Variables and Personal Information
+
+let firstName = "Elijah";
+let lastName = "Imaikop";
+let age = 25;
+let favoriteLanguage = "JavaScript";
+
+console.log(`Hi, my name is ${firstName} ${lastName}. I am ${age} years old and my favorite programming language is ${favoriteLanguage}.`);
+
+//Simple Calculator
+
+let num1 = 10;
+let num2 = 3;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Remainder:", num1 % num2);
+
+//Age Checker
+
+let ageCheck = 17;
+
+if (ageCheck >= 18) {
+  console.log("You are eligible");
+} else {
+  console.log("You are not eligible yet");
+}
